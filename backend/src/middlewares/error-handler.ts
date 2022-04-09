71eb6409551e9cedc6cf0b7d4359acf9db1d8129
Nikhil Sharma <nikhil.sk.sharma@gmail.com>
2022-04-09 21:04:00 +0530

    error handling middleware

---
+++ backend/src/middlewares/error-handler.ts
@@ -0,0 +1,22 @@
+import { Request, Response, NextFunction } from "express";
+import { CustomError } from "../errors/custom-error";
+import { StatusCodes } from "http-status-codes";
+
+export const errorHandler = (
+    err: Error,
+    req: Request,
+    res: Response,
+    next: NextFunction,
+) => {
+    if (err instanceof CustomError) {
+        if (err.statusCode === StatusCodes.BAD_REQUEST) {
+            console.warn(JSON.stringify(err.serializeErrors()), null, 2)
+        }
+        return res.status(err.statusCode).send(err.serializeErrors());
+    }
+
+    console.error(JSON.stringify({ msg: err.message, name: err.name, stack: err.stack }));
+
+    res.status(StatusCodes.BAD_REQUEST).send([{ message: 'something went wrong' }])
+
+}
