8e86002737f98d681c2ceac3c94a265350055f53
Nikhil Sharma <nikhil.sk.sharma@gmail.com>
2022-04-09 21:04:34 +0530

    validation request middleware

---
+++ backend/src/middlewares/validate-request.ts
@@ -0,0 +1,23 @@
+import { Request, Response, NextFunction } from "express";
+import { validationResult } from "express-validator";
+import { RequestValidationError } from "../errors/request-validation-error";
+
+export const validateRequest = (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        console.log(JSON.stringify({
+            path: req.path,
+            url: req.url,
+            errors: errors.array()
+        }))
+        throw new RequestValidationError(errors.array());
+    }
+
+    next();
+
+}
