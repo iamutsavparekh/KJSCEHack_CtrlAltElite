74d8b7567201120f14d16ec845c855963f309e49
Shreya Kulkarni <61427019+srk129@users.noreply.github.com>
2022-04-10 11:17:33 +0530

    Update contrast

--- frontend/models/contrast.py
+++ frontend/models/contrast.py
@@ -5,7 +5,3 @@ def adjust_contrast(input_image, output_image, factor):
     enhancer_object = ImageEnhance.Contrast(image)
     out = enhancer_object.enhance(factor)
     out.save(output_image)
-if __name__ == '__main__':
-    adjust_contrast('jpeg_trial.jpg',
-                    'contrast.jpg',
-                    1.7)
